5a962d61adfba408849d947641b67724d435aa3a
Server1567 <chapijala@outlook.com>
2017-05-14 16:19:09 -0400

    v2.2.6

--- about_me.php
+++ about_me.php
@@ -4,23 +4,15 @@
 
 	<h4 style="float: right; font-family: arial"><em>Fundado el 29 de septiembre de 2010</em></h4>
 <br><br><br>
+	<img src="res/imagen1.jpeg" alt="" style="width: 100%"><br><br><br>
 	<p>
-		El Coro Acción Comunitaria nació como el coro oficial de la parroquia San Rafael Arcángel de la comunidad 24 de abril el dia 29 de septiembre de 2010, fue fundado por el profesor Luis A. De la Cruz Martínez (Richard).
+		El Coro Acción Comunitaria nació como el coro oficial de la parroquia San Rafael Arcángel de la comunidad 24 de abril el dia 29 de septiembre de 2010, fue fundado por el profesor Luis A. De la Cruz Martínez (Richard).Para ese entonces su propósito era la exaltación y adoración a Dios y llevar a la comunidad el mensaje de evangelización a través de sus canciones, teniendo como apoyo al reverendo padre Joselito Beltré.
 	</p>
 	<p>
-		Para ese entonces su propósito era la exaltación y adoración a Dios y llevar a la comunidad el mensaje de evangelización a través de sus canciones, teniendo como apoyo al reverendo padre Joselito Beltré.
+		El coro ha cantado en varias parroquias de la provincia de Santo Domingo y el Distrito Nacional incluyendo La Catedral Primada de América, en el 2011 participó en el "Festival de Los Coros" realizado en el Parque Colón en conmemoración del Jubileo de los 500 años de la evangelización de Santo Domingo.El 1ro. de octubre de 2011 celebró su primer aniversario con un concierto titulado "Este Es El Día" donde fue invitado el coro Voces de San Martin de la Parroquia San Ignacio de Loyola del sector La Ciénaga.
 	</p>
 	<p>
-		El coro ha cantado en varias parroquias de la provincia de Santo Domingo y el Distrito Nacional incluyendo La Catedral Primada de América, en el 2011 participó en el "Festival de Los Coros" realizado en el Parque Colón en conmemoración del Jubileo de los 500 años de la evangelización de Santo Domingo.
-	</p>
-	<p>
-		El 1ro. de octubre de 2011 celebró su primer aniversario con un concierto titulado "Este Es El Día" donde fue invitado el coro Voces de San Martin de la Parroquia San Ignacio de Loyola del sector La Ciénaga.
-	</p>
-	<p>
-		En diciembre de ese mismo año llevó su voz a la Base Aérea de San Isidro cantando villancicos en el Club de Esposas de Oficiales.
-	</p>
-	<p>
-		Fue invitado por el Director del Coro Nacional Prof. Simón Cesar Nova para cantar en la misa de celebración del 57 aniversario de la fundación de dicho coro.
+		En diciembre de ese mismo año llevó su voz a la Base Aérea de San Isidro cantando villancicos en el Club de Esposas de Oficiales.Fue invitado por el Director del Coro Nacional Prof. Simón Cesar Nova para cantar en la misa de celebración del 57 aniversario de la fundación de dicho coro.
 	</p>
 	<p>
 		Participó en la 5ta. Versión del Reality Show "Mi Voz Para Cristo" llegando a la Gran Final celebrada el 22 de abril de 2012 en la Sala Principal del Teatro Nacional Eduardo Brito.
@@ -46,6 +38,11 @@
 	<p>
 		Los días 18 y 19 de junio de 2016 realizó dos conciertos en el municipio de Río San Juan de la provincia María Trinidad Sánchez en conmemoración de sus fiestas patronales presentándose allá en varias parroquias y en hogares de destacadas personalidades como lo es el compositor Juan Eddy Paulino; ese mismo año en Sánchez Samaná fue parte de la celebración de la acostumbrada actividad "Sancheros Ausentes", en donde ofreció un concierto.
 	</p>
+	<br>
+	<img src="res/imagen2.jpeg" alt="" style="width: 100%">
+	<br>
+	<br>
+	<br>
 	<p>
 		El 10 de diciembre de 2016 ofreció un concierto navideño en el parque Duarte auspiciado por el Ministerio de Cultura celebrando la "Noche Larga de Los Museos".
 	</p>
