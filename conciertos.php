d84f67cf9e6568ab000dd21c6af37824ef0b2a45
Server1567 <chapijala@outlook.com>
2017-05-17 12:09:26 -0400

    v2.3

--- conciertos.php
+++ conciertos.php
@@ -1,8 +1,6 @@
 	<div class="row">
 		<div id="view" class="view col-md-12 hide">
-			<video class="post" controls="true" id="videoView" preload>
-				<source src="res/videos/video_1.mp4" type="video/mp4">
-			</video>
+			<video type="video/mp4" src="res/videos/video_1.mp4" class="post" controls="true" id="videoView" preload autoplay="true">
 		</div>	
 	</div>
 
@@ -18,90 +16,66 @@
 		<section class="row">
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_1.png">
-						<source src="res/videos/video_1.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_1.mp4" class="post video" preload poster="res/poster_1.png">
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_2.png">
-						<source src="res/videos/video_2.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_2.mp4" class="post video" preload poster="res/poster_2.png">
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_1.png">
-						<source src="res/videos/video_3.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_3.mp4" class="post video" preload poster="res/poster_1.png">
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_2.png">
-						<source src="res/videos/video_4.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_4.mp4" class="post video" preload poster="res/poster_2.png">
 				</div>
 			</div>
 		</section>
 		<section class="row">
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_1.png">
-						<source src="res/videos/video_1.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_1.mp4" class="post video" preload poster="res/poster_1.png">
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_2.png">
-						<source src="res/videos/video_2.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_2.mp4" class="post video" preload poster="res/poster_2.png">
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_1.png">
-						<source src="res/videos/video_3.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_3.mp4" class="post video" preload poster="res/poster_1.png">
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_2.png">
-						<source src="res/videos/video_4.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_4.mp4" class="post video" preload poster="res/poster_2.png">
 				</div>
 			</div>
 		</section>
 		<section class="row">
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_1.png">
-						<source src="res/videos/video_1.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_1.mp4" class="post video" preload poster="res/poster_1.png">
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_2.png">
-						<source src="res/videos/video_2.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_2.mp4" class="post video" preload poster="res/poster_2.png">
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_1.png">
-						<source src="res/videos/video_3.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_3.mp4" class="post video" preload poster="res/poster_1.png">
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-3" id="video-container">
 				<div class="thumbnail">
-					<video class="post video" controls="true" preload poster="res/poster_2.png">
-						<source src="res/videos/video_4.mp4" type="video/mp4" class="src">
-					</video>
+					<video type="video/mp4" src="res/videos/video_4.mp4" class="post video" preload poster="res/poster_2.png">
 				</div>
 			</div>
 		</section>
@@ -110,13 +84,9 @@
 		
 		$(document).ready(function(){			
 			$(".video").click(function(){
-				var attr = $("video source:first-child").attr("src");
-				//$("#view").attr("class", "view col-md-12 active");
-				console.log(attr);
-
-				var alert = document.getElementById("alert");
-
-				alert.setAttribute("class", "alert alert-warning alert-dismissible");
+				var attr = $(this).attr("src");
+				$("#view").attr("class", "view col-md-12 active");
+				$("#view #videoView").attr("src", attr);
 			});
 		});
 
